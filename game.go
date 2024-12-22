2a8b0030c6b51b98523b6870789a432eb510e1cd
Joe Biwer <josephbiwer@gmail.com>
2024-12-21 20:15:49 -0600

    init commit

---
+++ game.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+    "fmt"
+    "os"
+    "os/exec"
+)
+
+func main() {
+    renderer.test()
+
+    // Clear the screen
+    cmd := exec.Command("clear")
+    cmd.Stdout = os.Stdout
+    cmd.Run()
+
+    fmt.Println("\033[20;40HTest")
+}
